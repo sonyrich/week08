ecd9aa44d0f1f2785093f51e188f13e501d69261
sony_rich <hoangson200015@gmail.com>
2026-09-11 06:09:54 +1000

    Update Login.test.jsx

--- frontend/src/test/Login.test.jsx
+++ frontend/src/test/Login.test.jsx
@@ -40,7 +40,7 @@ describe("Login page", () => {
 
     expect(
       screen.getByText(
-        "KoalaTech University"
+        "KoalaTech University Login 9.3C CD Demo"
       )
     ).toBeInTheDocument();
 
